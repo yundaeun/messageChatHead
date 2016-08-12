4c6e752f18409e4d1bd840131fa399b6bc63249a
daeun yun <Naver@AD00037920.local>
2016-08-12 15:18:55 +0900

    GestureDetector 추가

--- app/src/main/java/com/example/naver/messagechathead/ChatBubbleCreator.java
+++ app/src/main/java/com/example/naver/messagechathead/ChatBubbleCreator.java
@@ -1,13 +1,10 @@
 package com.example.naver.messagechathead;
 
-import static android.view.GestureDetector.*;
-
 import android.content.Context;
 import android.graphics.PixelFormat;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.GestureDetector;
-import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -43,10 +40,12 @@ public class ChatBubbleCreator extends LinearLayout implements View.OnTouchListe
 		layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		faceIcon = layoutInflater.inflate(R.layout.face_icon_layout, null);
 		faceIconParams = attachLayout(faceIcon, Gravity.START | Gravity.TOP, View.VISIBLE, faceIconSize, faceIconSize, WindowManager.LayoutParams.TYPE_PRIORITY_PHONE);
-		faceIcon.setOnTouchListener(this);
 
+		// move action
+		faceIcon.setOnTouchListener(this);
 		// click listener
 		gestureDetector = new GestureDetector(context, new SimpleGestureListener());
+		// delete action
 		chatBubbleDeleter = new ChatBubbleDeleter(context, windowManager);
 
 	}
@@ -70,12 +69,6 @@ public class ChatBubbleCreator extends LinearLayout implements View.OnTouchListe
 		return params;
 	}
 
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		Log.d("Yde", "yde on onTouchEvent");
-		
-		return gestureDetector.onTouchEvent(event);
-	}
 
 	@Override
 	public boolean onTouch(View v, MotionEvent event) {
@@ -107,17 +100,9 @@ public class ChatBubbleCreator extends LinearLayout implements View.OnTouchListe
 					break;
 			}
 		}
+		gestureDetector.onTouchEvent(event);
 		return false;
 	}
-
-
-
-
-
-
-
-
-
 
 	// 자유로운 상태 (chatRoom이 닫힌 상태)
 	// 동작 : 버블이 모여서 함께 움직임
@@ -128,19 +113,14 @@ public class ChatBubbleCreator extends LinearLayout implements View.OnTouchListe
 	// 동작 : 하나씩 이동 가능
 	// 애니 : up 시 제자리로 이동
 
-
 	// 삭제 버튼..
 }
 
 class SimpleGestureListener extends GestureDetector.SimpleOnGestureListener {
 
 	@Override
-	public boolean onContextClick(MotionEvent e) {
-		Log.d("yde", "yde onContextClick");
-		return super.onContextClick(e);
-	}
-	public boolean onDown(MotionEvent e) {
-		Log.d("yde", "yde odddnclick");
+	public boolean onSingleTapConfirmed (MotionEvent e) {
+
 		return true;
 	}
 }
